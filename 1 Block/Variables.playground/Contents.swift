b8305a71d4a7f67e8f9a8426c1b2d555c46d6ec8
tk4chenko <tkachhhenko@gmail.com>
2022-09-27 23:08:31 +0300

    add variables practice

---
+++ 1 Block/Variables.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+
+var greeting = "Hello!"
+greeting = "Some text"
+
+
+var myBestFriendsGreeting = "Hi, bro!"
+
+
+let name: String = "Bob"
+var age1: Int = 28
+var isMerried = true
+
+age1 = 10
+
+isMerried = false
+
+var a = 10
+var b = 15
+
+let sum = a + b
+let mult = a * b
+let myLine = "Sum is - \(sum), multiply result is - \(mult)!!!"
+print(myLine)
+
+print("Number of symbols in my line is: \(myLine.count)")
+isMerried.toggle()
+print(isMerried)
+
+
+var age2 = 18
+//print("\(age), 2004 year of birth")
+var isAtHome = true
+var jobTitle = "iOS Developer"
+jobTitle = "Senior \(jobTitle)"
+
+print("I'm \(age2), I work in a position \(jobTitle)!")
