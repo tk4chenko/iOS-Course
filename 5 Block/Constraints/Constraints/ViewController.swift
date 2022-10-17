d50dc583d10d351572bfbbac5d0855850f1a3069
tk4chenko <tkachhhenko@gmail.com>
2022-10-17 09:42:01 +0300

    add constraints practice

---
+++ 5 Block/Constraints/Constraints/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Artem Tkachenko on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var someView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
