c7ca2beab51937677cacec6ac7b9b0bcaa834138
tk4chenko <tkachhhenko@gmail.com>
2022-10-18 16:18:17 +0300

    add ui in storyboard

---
+++ 5 Block/UI in storyboard/UI in storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI in storyboard
+//
+//  Created by Artem Tkachenko on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
